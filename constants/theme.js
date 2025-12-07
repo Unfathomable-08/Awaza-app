ab89c953383f378fe6007cb94fdfd238cb504c3a
Muhammad <muhammad124711@gmail.com>
2025-12-07 15:13:00 +0500

    theme

--- constants/theme.js
+++ constants/theme.js
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
-    primary: '#00C26F',
-    primaryDark: '#00AC62',
+    primary: '#EE3434',
+    primaryDark: '#E14C50',
     dark: '#3E3E3E',
     darkLight: '#E1E1E1',
     gray: '#E3E3E3',
